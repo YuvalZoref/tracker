22fa169ac5c2f29a8b137e1f06c9b6c7efac0456
yuvalzoref <yuval_zoref@hotmail.com>
2023-02-15 16:34:34 +0000

    added controllers & views

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,9 @@
+class BookingsController < ApplicationController
+
+  def my_bookings
+  end
+
+  def all_bookings
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+
+  get "dashboard", to: "pages#dashboard"
+
+  get "my_bookings", to: "bookings#my_bookings"
+
+  get "all_bookings", to: "bookings#all_bookings"
+
 end
